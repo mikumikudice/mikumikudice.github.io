02f3d03a2b7e1bf45c5807f99552571bfdd4f02a
mateusmds <mateusmoraisdias3@gmail.com>
2021-04-27 18:19:56 -0300

    fixed page 404 footnote

---
+++ .lide_workspace.lua
@@ -0,0 +1,1 @@
+return {["active_view"]=1,["views"]={[1]={["text"]=false,["filename"]="404.html",["active"]=true,["selection"]={[1]=25,[2]=13,[3]=25,[4]=13},["scroll"]={["y"]=100,["x"]=0},["type"]="doc"}},["type"]="leaf"}
