6bed1412b8f0466f316d2c038bf1d1f4232e1da8
mateusmds <mateusmoraisdias3@gmail.com>
2021-04-27 18:23:22 -0300

    fixed again the 404 page

--- .lide_workspace.lua
+++ .lide_workspace.lua
@@ -1,1 +1,1 @@
-return {["active_view"]=1,["views"]={[1]={["text"]=false,["filename"]="404.html",["active"]=true,["selection"]={[1]=25,[2]=13,[3]=25,[4]=13},["scroll"]={["y"]=100,["x"]=0},["type"]="doc"}},["type"]="leaf"}
+return {["active_view"]=1,["type"]="leaf",["views"]={[1]={["active"]=true,["filename"]="404.html",["selection"]={[1]=13,[2]=1,[3]=13,[4]=1},["scroll"]={["x"]=0,["y"]=0},["type"]="doc",["text"]=false}}}
