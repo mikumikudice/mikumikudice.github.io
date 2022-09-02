13a6cf0a699d4cbb0ac5e9c0ae7d04fd994a6120
mateus-md <mateusmoraisdias3@gmail.com>
2022-09-02 15:34:24 -0300

    + added mail-to redirect on footer email

--- assets/js/auto_header.js
+++ assets/js/auto_header.js
@@ -16,7 +16,7 @@ function init_header(){
         all images' rights are reserved.<br/>
         this website uses the color pallete <a href = "https://lospec.com/palette-list/laser-lab" target = "_blank">laser lab</a> made by <a href = "https://twitter.com/polyphorge" target = "_blank">polyphorge.</a></br></br>
         <address>
-            contact email: 6s4aq0np7@mozmail.com |
+            contact email: <a href = "mailto:6s4aq0np7@mozmail.com">6s4aq0np7@mozmail.com</a> |
             twitter: <a href = "https://twitter.com/levi_alfeuson" target = "_blank">
             @levi_alfeuson</a>
         </address>
