a421f4877cf88a1a4bfb09fe47fca83366ffcd47
mmd <mateusmoraisdias3@gmail.com>
2022-12-16 15:28:01 -0300

    updated twitter account link to new user

--- assets/js/auto_header.js
+++ assets/js/auto_header.js
@@ -17,7 +17,7 @@ function init_header(){
         this website uses the color pallete <a href = "https://lospec.com/palette-list/laser-lab" target = "_blank">laser lab</a> made by <a href = "https://twitter.com/polyphorge" target = "_blank">polyphorge</a>.</br></br>
         <address>
             contact email: <a href = "mailto:6s4aq0np7@mozmail.com">6s4aq0np7@mozmail.com</a> |
-            twitter: <a href = "https://twitter.com/levi_alfeuson" target = "_blank">@levi_alfeuson</a> |
+            twitter: <a href = "https://twitter.com/mikumikudice" target = "_blank">@mikumikudice</a> |
             mastodon: <a rel="me" href="https://social.linux.pizza/@mikumikudice"@mikumikudice@social.linux.pizza</a>
         </address>
     `;
