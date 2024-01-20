8a61d66bbecb8c67cd4512db90b2ed845824d053
mateusmd <mikomdias@yahoo.com>
2024-01-20 14:19:57 -0300

    debug session for elm init bug #3

--- src/Main.elm
+++ src/Main.elm
@@ -94,29 +94,6 @@ init _ url key =
         baseurl = get_base url
         bad = url.fragment
     in
-    case bad of
-        Just res ->
-            if ( String.startsWith "badurl_" res ) then
-                let
-                    failed = String.replace "badurl_" "" res
-                    _ = Debug.log "string" "worked!"
-                in
-                ( Model key "/404" baseurl ( div [] [ text "failed to load homepage :c" ] ) ( div [] [ text "failed to load the footer :c" ] )
-                , Cmd.batch
-                    [ Nav.pushUrl key (String.concat [ baseurl, failed ] )
-                    , fetch baseurl "/404"
-                    , fetch baseurl "/footnote"
-                    ]
-                )
-            else
-                ( Model key "/home" baseurl ( div [] [ text "failed to load homepage :c" ] ) ( div [] [ text "failed to load the footer :c" ] )
-                , Cmd.batch
-                    [ Nav.pushUrl key baseurl
-                    , fetch baseurl "/footnote"
-                    ]
-                )
-        Nothing ->
-            let _ = Debug.log "string" "did not work!" in
             ( Model key "/home" baseurl ( div [] [ text "failed to load homepage :c" ] ) ( div [] [ text "failed to load the footer :c" ] )
             , Cmd.batch
                 [ Nav.pushUrl key (String.concat [ baseurl, "/home" ] )
